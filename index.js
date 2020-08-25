c2268b9a5540e37eb3bf6a52ced224a40cdfa2ef
Paul Kim <khyunwoo1@gmail.com>
2020-08-25 01:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  
+  var emptyArray = [];
+  var emptyString = "";
+
+  for (var i = 0; i < musicians.length; i++) {
+    emptyString = musicians[i] + " plays " + instruments[i];
+    emptyArray.push(emptyString) 
+  }
+  
+  return emptyArray;
+  
+  
+}
+  
+
+
+function johnLennonFacts(lennonArray){
+  
+  var myString = '';
+  var myArray = [];
+
+
+  var i = 0;
+  while(i < lennonArray.length) {
+    
+    myString = (lennonArray[i] + '!!!');
+
+
+    
+    myArray.push(myString);
+    i++;
+}
+  return myArray;
+  
+}
+
+function johnLennonFacts(number){
+  
+  var emptyArrayTwo = [];
+  var i = 0;
+
+  if (number < 15){
+  
+      do{
+          i++;
+          
+          emptyArrayTwo += "I love the Beatles!"
+  
+      
+      
+      } while(i < number)
+  
+    return emptyArrayTwo;
+  }
+  
+}
+
+
+
